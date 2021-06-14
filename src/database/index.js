14d889f9025024b9055f1f58e02f4cfd064b5280
huycans <huycan_lk@yahoo.com.vn>
2021-06-14 15:12:40 -0400

    finished age API

--- src/database/index.js
+++ src/database/index.js
@@ -35,11 +35,41 @@ const getHobbies = () => {
 
 const getListOfAgesOfUsersWith = (hobby) => {
   const dataAccessMethod = () => {
-    // fill me in :) should return an arry of age count based on hobby.
-    return [
-      { age: 18, count: 2 },
-      { age: 12, count: 1 },
-    ];
+    //should return an arry of age count based on hobby.
+    //assume that every user has a different name
+
+    let usersWithHobby = [];
+
+    //filter out users who do not have hobby
+    for (let user in db.hobbiesOfUserByUsername) {
+      if (db.hobbiesOfUserByUsername[user].indexOf(hobby) !== -1) {
+        usersWithHobby.push(user);
+      }
+    }
+
+    //count the age of the users with hobby
+    let ageCount = {};
+    for (let userId in db.usersById) {//for every user info object in db
+      const userInfo = db.usersById[userId];
+      if (usersWithHobby.indexOf(userInfo.username) !== -1) {//if user has the same name as user with hobby
+        if (!ageCount[userInfo.age]) {//and if user's age group has not been assigned yet
+          ageCount[userInfo.age] = 1;
+        }
+        else {//found the age group of this user
+          ageCount[userInfo.age] += 1;
+        }
+      }
+    }
+
+    //convert ageCount object to array
+    let result = [];
+    for (let age in ageCount) result.push({ age: +age, count: ageCount[age] });
+
+    return result;
+    // return [
+    //   { age: 18, count: 2 },
+    //   { age: 12, count: 1 },
+    // ];
   };
   return mockDBCall(dataAccessMethod);
 };
