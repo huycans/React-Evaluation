816fd3c8fd1334055cadf105c38e4f0a5b54d1f1
huycans <huycan_lk@yahoo.com.vn>
2021-06-15 17:05:32 -0400

    Split app into 2 components

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { getUsers, getHobbies, getListOfAgesOfUsersWithHobby } from './api/API';
+import UserInfo from './components/UserInfo';
+import HobbyCount from "./components/HobbyCount";
+
 import './SCSS_styles.scss';
 
 const App = () => {
@@ -31,66 +34,14 @@ const App = () => {
         rej => console.log(rej));
   }, []);
 
-  const usernameAndAge = (userInfo === null)
-    ? null
-    : userInfo.map(user => (
-      <tr key={user.name}>
-        <td className="text-left">{user.username}</td>
-        <td className="text-right">{user.age}</td>
-      </tr>));
-
-  const hobbyOptions = (hobbies === null)
-    ? null
-    : hobbies.map(hobby => <option key={hobby} value={hobby}>{hobby}</option>);
-
-  const hobbyCountByAgeDisplay = (hobbiesCountByAge === null)
-    ? null
-    : <table className="table table-striped age-group">
-      <thead className="thead-dark">
-        <tr>
-          <th className="text-left" scope="col">Age</th>
-          <th className="text-right" scope="col">Count</th>
-        </tr>
-      </thead>
-      <tbody>
-        {hobbiesCountByAge.map(ageGroup => <tr key={ageGroup.age}>
-          <td className="text-left">{ageGroup.age}</td>
-          <td className="text-right">{ageGroup.count}</td>
-        </tr>)}
-      </tbody>
-    </table>;
-
-
   return (
     <div className="app container">
-      <div className="user-info d-flex row justify-content-center flex-column text-center align-items-center">
-        <h1>All users</h1>
-        <h4>username and age</h4>
-        <table className="table table-striped username">
-          <thead className="thead-dark">
-            <tr>
-              <th className="text-left" scope="col">Username</th>
-              <th className="text-right" scope="col">User age</th>
-            </tr>
-          </thead>
-          <tbody>
-            {usernameAndAge}
-          </tbody>
-        </table>
-      </div>
-      <div className="user-hobby d-flex row justify-content-center flex-column text-center align-items-center">
-        <h1>Age demographic of users with hobby</h1>
-        <div className="d-flex row justify-content-center flex-column text-center align-items-center">
-          <label htmlFor="hobby">Hobby:</label>
-          <select className="text-center"
-            value={selectedHobby} onChange={handleSelect}
-            name="hobby" id="hobby">
-            <option value="Hobby">Hobby</option>
-            {hobbyOptions}
-          </select>
-        </div>
-        {hobbyCountByAgeDisplay}
-      </div>
+      <UserInfo userInfo={userInfo} setUserInfo={setUserInfo}></UserInfo>
+      <HobbyCount handleSelect={handleSelect}
+        hobbies={hobbies}
+        hobbiesCountByAge={hobbiesCountByAge}
+        selectedHobby={selectedHobby}
+      ></HobbyCount>
     </div>
   );
 };
---
+++ src/client/src/components/UserInfo.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const UserInfo = (props) => {
+  // let [userInfo, setUserInfo] = useState(null);
+  const { userInfo, setUserInfo } = props;
+
+  const usernameAndAge = (userInfo === null)
+    ? null
+    : userInfo.map(user => (
+      <tr key={user.name}>
+        <td className="text-left">{user.username}</td>
+        <td className="text-right">{user.age}</td>
+      </tr>));
+
+  return (
+    <div className="app container">
+      <div className="user-info d-flex row justify-content-center flex-column text-center align-items-center">
+        <h1>All users</h1>
+        <h4>username and age</h4>
+        <table className="table table-striped username">
+          <thead className="thead-dark">
+            <tr>
+              <th className="text-left" scope="col">Username</th>
+              <th className="text-right" scope="col">User age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {usernameAndAge}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default UserInfo;
